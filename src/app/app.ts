dd48826be6cbfa1f09ceb45a238e0256cacc1335
15138730370 <1037483576@qq.com>
2017-12-30 08:20:08 +0800

    save to back

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { Base64Service } from './base64';
-import { Base64LibService } from './base64-lib';
 
 @NgModule({
-    providers: [Base64Service, Base64LibService]
+    providers: [
+        Base64Service
+    ]
 })
 export class Base64Module { }
 export { Base64Service } from './base64';
+
--- src/app/base64.ts
+++ src/app/base64.ts
@@ -1,15 +1,47 @@
 import { Injectable } from '@angular/core';
-import { Base64LibService } from './base64-lib';
 @Injectable()
 export class Base64Service {
-    time: any = new Date().getTime();
-    constructor(
-        public base64: Base64LibService
-    ) {
-        console.log('base64 service', this.time);
-    }
+    base64DecodeChars = new Array(
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
+        52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1,
+        -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+        15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,
+        -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+        41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1
+    );
+    base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    constructor() { }
 
-    encode(s: string): string {
-        return this.base64.encode(s);
+    encode(str: string): string {
+        var out, i, len;
+        var c1, c2, c3;
+        len = str.length;
+        i = 0;
+        out = "";
+        while (i < len) {
+            c1 = str.charCodeAt(i++) & 0xff;
+            if (i == len) {
+                out += this.base64EncodeChars.charAt(c1 >> 2);
+                out += this.base64EncodeChars.charAt((c1 & 0x3) << 4);
+                out += "==";
+                break;
+            }
+            c2 = str.charCodeAt(i++);
+            if (i == len) {
+                out += this.base64EncodeChars.charAt(c1 >> 2);
+                out += this.base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+                out += this.base64EncodeChars.charAt((c2 & 0xF) << 2);
+                out += "=";
+                break;
+            }
+            c3 = str.charCodeAt(i++);
+            out += this.base64EncodeChars.charAt(c1 >> 2);
+            out += this.base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+            out += this.base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >> 6));
+            out += this.base64EncodeChars.charAt(c3 & 0x3F);
+        }
+        return out;
     }
 }
